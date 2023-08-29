2420e1f2620de53aab9f9a784728aeba280e120c
Imhim1234567 <133804501+Imhim1234567@users.noreply.github.com>
2023-08-29 10:19:07 -0400

    Update deck.js

--- deck.js
+++ deck.js
@@ -32,6 +32,7 @@ class Card{
     for (let i = this.cards.length - 1; i > 0; i--){
       const newIndex = Math.floor(Math.random() * (i + 1))
       const oldlValue this.cards[newIndex]
+      this.cards[newIndex] = this.cards[i]
       this.cards[i] = oldValue
     }
   }
