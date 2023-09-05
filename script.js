00a9e327117dd5302e9f1b7fb97283e7e93efce0
Imhim1234567 <133804501+Imhim1234567@users.noreply.github.com>
2023-09-05 10:28:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const deck = new Deck()
 deck.shuffle()
 
   const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
-  playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
-  computerDeck  new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
+  playerDeck = Deck(deck.cards.slice(0, deckMidpoint))
+  computerDeck  Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
 console.log(playerDeck)
 console.log(computerDeck)
 }
