70ba3d32f0a989da360f64ecda2e0bfec2ce1c30
Imhim1234567 <133804501+Imhim1234567@users.noreply.github.com>
2023-09-05 10:24:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 import DECK from './deck.js'
 
 const computerCardSlot = document.querySelector('.computer-card-slot')
+let playerDeck, computerDeck
 function startGame(){
 const deck = new Deck()
 deck.shuffle()
 
   const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
-  playerDeck
-  computerDeck
+  playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
+  computerDeck  new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
 }
 computerCardSlot.appendChild(deck.cards[0].getHTML())
