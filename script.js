f969dbdb45581e93607feb9230d58691759ad3dd
Imhim1234567 <133804501+Imhim1234567@users.noreply.github.com>
2023-09-08 10:47:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 import DECK from './deck.js'
 
 const computerCardSlot = document.querySelector('.computer-card-slot')
+const playerCardSlot = document.querySelector('.player-card-slot')
+const computerDeck = document.querySelector('.computer-deck')
+const computerDeck = document.querySelector('.computer-deck')
+const playerDeck = document.querySelector('.player-deck)
+const computerCardSlot = document.querySelector('.computer-card-slot')
+const computerDeck = document.querySelector('.computer-deck')                                          
 let playerDeck, computerDeck
 
 // function to be able to play the game 
@@ -12,7 +18,8 @@ deck.shuffle()
   playerDeck =  new Deck(deck.cards.slice(0, deckMidpoint))
   computerDeck   new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
   
-console.log(playerDeck)
-console.log(computerDeck)
+cleanBeforeRound()
 }
+
+function cleanBeforeRound()
 
