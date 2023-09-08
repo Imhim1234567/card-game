13c707796f1adfd6688122b37c0ad8166ca0d184
Imhim1234567 <133804501+Imhim1234567@users.noreply.github.com>
2023-09-08 10:02:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ import DECK from './deck.js'
 
 const computerCardSlot = document.querySelector('.computer-card-slot')
 let playerDeck, computerDeck
+
+// function to be able to play the game 
 function startGame(){
 const deck = new Deck()
 deck.shuffle()
-
+// splits the cards into 2 equal halfs
   const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
   playerDeck =  new Deck(deck.cards.slice(0, deckMidpoint))
   computerDeck   new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
