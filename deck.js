1b5a84b9335d9fe0760888cc0a1011e7b6261802
Imhim1234567 <133804501+Imhim1234567@users.noreply.github.com>
2023-09-12 06:00:36 -0400

    Update deck.js

--- deck.js
+++ deck.js
@@ -1,61 +1,70 @@
 const SUITS = ["♠", "♣", "♥", "♦"]
-const VALUES = ["A",
-                "2",
-                "3",
-                "4",
-                "5",
-                "6",
-                "7",
-                "8",
-                "9",
-                "10',
-                "J",
-                "Q",
-                "K"
-              ]
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+]
 
 export default class Deck {
-  constructor(cards = freshDeck()){
+  constructor(cards = freshDeck()) {
     this.cards = cards
   }
+
   get numberOfCards() {
     return this.cards.length
   }
-// the function shuffles the cards
-   shuffle(){
-    for (let i = this.numberOfCards - 1; i > 0; i--){
+
+  pop() {
+    return this.cards.shift()
+  }
+
+  push(card) {
+    this.cards.push(card)
+  }
+
+  shuffle() {
+    for (let i = this.numberOfCards - 1; i > 0; i--) {
       const newIndex = Math.floor(Math.random() * (i + 1))
-      const oldlValue this.cards[newIndex]
+      const oldValue = this.cards[newIndex]
+      this.cards[newIndex] = this.cards[i]
       this.cards[i] = oldValue
     }
   }
 }
 
-class Card{
-  constructor(suit, value){
+class Card {
+  constructor(suit, value) {
     this.suit = suit
     this.value = value
   }
-  get color(){
-    return this.suit === "♣" || this.suit === "♠" ? 'black': 'red'
+
+  get color() {
+    return this.suit === "♣" || this.suit === "♠" ? "black" : "red"
   }
-  
-// this is supposed randomly select a card from the from the array of cards
-   getHTML(){
+
+  getHTML() {
     const cardDiv = document.createElement("div")
     cardDiv.innerText = this.suit
     cardDiv.classList.add("card", this.color)
     cardDiv.dataset.value = `${this.value} ${this.suit}`
     return cardDiv
   }
- 
 }
-
 
 function freshDeck() {
   return SUITS.flatMap(suit => {
     return VALUES.map(value => {
       return new Card(suit, value)
-   })
+    })
   })
 }
